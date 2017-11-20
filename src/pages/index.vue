9cfe9c93738dd80c45b4734abb9de30a165c3bd8
刘阳 <place222@hotmail.com>
2017-11-20 23:22:00 +0800

    添加less支持

---
+++ src/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <!-- Main Header -->
+    <main-header></main-header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <main-sidebar></main-sidebar>
+
+    <!-- Content Wrapper. Contains page content -->
+    <main-content></main-content>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <main-footer></main-footer>
+
+    <!-- Control Sidebar -->
+    <control-sidebar></control-sidebar>
+    <!-- /.control-sidebar -->
+    <!-- Add the sidebar's background. This div must be placed
+    immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
+  </div>
+</template>
+
+<script>
+import mainHeader from "@/pages/header/mainHeader";
+import mainSidebar from "@/pages/sidebar/mainSidebar";
+import mainContent from "@/pages/content/mainContent";
+import mainFooter from "@/pages/footer/mainFooter";
+import controlSidebar from "@/pages/sidebar/controlSidebar";
+
+export default {
+  components: {
+    mainHeader,
+    mainSidebar,
+    mainContent,
+    mainFooter,
+    controlSidebar
+  }
+};
+</script>
